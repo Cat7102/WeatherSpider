75d90c63aeb6fb2664555755b075cb03fb2403ab
Fishercat <fishercat702@foxmail.com>
2022-05-01 16:51:41 +0800

    改善了excel写入性能

--- ExcelOperator.py
+++ ExcelOperator.py
@@ -3,6 +3,10 @@
 # Excel操作文件，包含了气象参数爬取所需要的excel步骤
 
 import os,logging,openpyxl
+import xlrd
+import xlwt
+from xlutils.copy import copy
+import logging
 
 def check_xlsx(filename):
     """
@@ -46,3 +50,39 @@ def append_xlsx(filename,sheetname,weatherdata):
     for i in range(len(weatherdata)):
         table.cell(nrows+1,i+1).value=weatherdata[i]
     workbook.save(filename)
+
+def append_xlsx2(filename,weatherdata):
+    """
+    在污染物excel文件中进行追加写入,使用openpyxl，更改了下代码提高性能
+    :param filename: excel文件名
+    :param weatherdata: 气象数据list
+    :return: None
+    """
+    workbook = openpyxl.load_workbook(filename)
+    for weatherlist in weatherdata:
+        station=weatherlist[0]
+        table = workbook[station]
+        table.append(weatherlist[1::])
+        logging.info(station+"信息已写入")
+    workbook.save(filename)
+
+def append_xlsx_xlwt(filename,weatherdata):
+    """
+    在污染物excel文件中进行追加写入,使用xlwt库，效率更高，但是只支持xls文件
+    :param filename: excel文件名
+    :param weatherdata: 气象数据list
+    :return: None
+    """
+    wb=xlrd.open_workbook(filename)
+    excel=copy(wb) # 对复制的对象进行操作
+    for weatherlist in weatherdata:
+        station=weatherlist[0]
+        nrows = wb.sheet_by_name(station).nrows # 获取最大行数
+        sheet_index = [s.name for s in wb.sheets()].index(station)
+        sheet = excel.get_sheet(sheet_index)
+        c=0
+        for value in weatherlist[1::]:
+            sheet.write(nrows, c, value)
+            c += 1
+        logging.info(station+"信息已写入")
+    excel.save(filename)
--- main.py
+++ main.py
@@ -14,22 +14,30 @@ filename="chem_data.xlsx"
 update_time=ChemSpider.get_last_updatetime(filename,"普陀监测站")    # 获取上一次更新时间
 logging.info("上一次更新时间为：{}".format(update_time))
 while True:
-    #proxy=ProxySearch.proxyserch()
-    weather_list, update_time = ChemSpider.get_weather(update_time)
-    stationname_list = []
-    if weather_list!=False:
-        for i in weather_list:
-            stationname_list.append(i[0])
-        result=ExcelOperator.check_xlsx(filename)
-        if result==False:
-            ExcelOperator.creat_xlsx(filename,stationname_list)
-            for i in range(len(stationname_list)):
-                ExcelOperator.append_xlsx(filename,stationname_list[i],weather_list[i][1::])
-                logging.info(stationname_list[i] + "信息已写入")
-        elif result==True:
-            for i in range(len(stationname_list)):
-                ExcelOperator.append_xlsx(filename,stationname_list[i],weather_list[i][1::])
-                logging.info(stationname_list[i] + "信息已写入")
-    logging.info("本轮信息更新完成")
-    time.sleep(60 * 30)
+    try:
+        #proxy=ProxySearch.proxyserch()
+        weather_list, update_time = ChemSpider.get_weather(update_time)
+        stationname_list = []
+        if weather_list!=False:
+            for i in weather_list:
+                stationname_list.append(i[0])
+            result=ExcelOperator.check_xlsx(filename)
+            if result==False:
+                ExcelOperator.creat_xlsx(filename,stationname_list)
+                for i in range(len(stationname_list)):
+                    ExcelOperator.append_xlsx(filename,stationname_list[i],weather_list[i][1::])
+                    logging.info(stationname_list[i] + "信息已写入")
+            elif result==True:
+                ''' 老方法
+                for i in range(len(stationname_list)):
+                    ExcelOperator.append_xlsx(filename,stationname_list[i],weather_list[i][1::])
+                    logging.info(stationname_list[i] + "信息已写入")
+                '''
+                ExcelOperator.append_xlsx2(filename,weather_list)
+        logging.info("本轮信息更新完成")
+        time.sleep(60 * 30)
+    except:
+        logging.info("网络暂时无法连接，1分钟后重试")
+        time.sleep(60)
+
 
